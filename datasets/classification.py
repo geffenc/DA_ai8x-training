c8ddc7142d6656637ab46ad2b3a08a8d61bed0b6
Geffen Cooper <geffen.cooper@maximintegrated.com>
2022-06-30 14:02:50 -0500

    generated group g1

--- datasets/classification.py
+++ datasets/classification.py
@@ -39,6 +39,10 @@ import os
 from matplotlib import cm
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+from math import comb
+
+from torch.utils.data.sampler import Sampler
+import random
 
 import ai8x
 
@@ -52,13 +56,11 @@ Parameters:
                  subdirectory basenames are the desired name of the object class.
                  i.e. dog/dog1.png, cat/cat1.png, etc.
   transform -    Specifies the image format (size, RGB, etc.) and augmentations to use
-  normalize -    Specifies whether to make the image zero mean, unit variance
 '''
 class ClassificationDataset(Dataset):
-    def __init__(self,img_dir_path,transform,normalize):
+    def __init__(self,img_dir_path,transform):
         self.img_dir_path = img_dir_path
         self.transform = transform
-        self.normalize = normalize
 
         print(self.img_dir_path)
         
@@ -103,9 +105,6 @@ class ClassificationDataset(Dataset):
             # apply any transformation
             if self.transform:
                 img = self.transform(img)
-                if self.normalize:
-                    norm = torchvision.transforms.Normalize((torch.mean(img)),(torch.std(img)))
-                    img = norm(img)
             
             # get the label
             label = self.labels[idx]
@@ -216,6 +215,229 @@ class ClassificationDataset(Dataset):
                     print(sample)
                     return
         plt.savefig("incorrect.png")
+
+
+'''
+Dataset Class for generating image pairs for few shot domain adaptation
+Parameters:
+  source_img_dir_path - Full path to directory with the source domain images.
+                        This assumes that the subdirectories contain each class, 
+                        only images are in these subdirectories, and that the
+                        subdirectory basenames are the desired name of the object class.
+                        i.e. dog/dog1.png, cat/cat1.png, etc.
+  target_img_dir_path - same as source_img_dir_path except for the target domain. The class
+                        names should be identical.
+  transform -           Specifies the image format (size, RGB, etc.) and augmentations to use
+  normalize -           Specifies whether to make the image zero mean, unit variance
+'''
+class DomainAdaptationPairDataset(Dataset):
+    def __init__(self,source_img_dir_path,target_img_dir_path,transform,shot):
+        self.source_img_dir_path = source_img_dir_path
+        self.target_img_dir_path = target_img_dir_path
+        self.transform = transform
+        self.shot = shot
+
+        print(self.source_img_dir_path)
+        print(self.target_img_dir_path)
+        
+        # collect img classes from source  dir names
+        source_img_classes = next(os.walk(source_img_dir_path))[1]
+        target_img_classes = next(os.walk(target_img_dir_path))[1]
+        if not source_img_classes == target_img_classes:
+            raise AssertionError("source and target classes not the same")
+        
+        self.source_dataset = ClassificationDataset(source_img_dir_path,transform)
+        self.target_dataset = ClassificationDataset(target_img_dir_path,transform)
+
+        self.s_sampler = EvenSampler(self.source_dataset)
+        self.t_sampler = EvenSampler(self.target_dataset,shot=shot) # only use a subset of the target dataset based on shot
+
+        # calculate the number of possible pairs for each set
+        # G1: same domain, same class --> each source sample can be paired with each source sample of the same class
+        self.num_G1_pairs = sum([comb(self.s_sampler.class_idx_lens[i],2) for i in range(self.s_sampler.num_classes)])
+        # G2: different domain, same class --> each target sample can be paired with each source sample of the same class
+        self.num_G2_pairs = sum([self.s_sampler.class_idx_lens[i]*self.t_sampler.class_idx_lens[i] for i in range(self.s_sampler.num_classes)])
+        # G3: same domain, different class --> each source sample can be paired with any other source sample
+        self.num_G3_pairs = comb(len(self.s_sampler.labels),2)
+        # G4: different domain, different class --> each target sample can be paired with each source sample of a different class
+        self.num_G4_pairs = sum([sum(
+                                    [self.s_sampler.class_idx_lens[j]*self.t_sampler.class_idx_lens[i] for j in range(self.s_sampler.num_classes) if i != j]
+                                        ) for i in range(self.t_sampler.num_classes)])
+
+        # this will always be the min, use as a reference
+        self.min_pairs = self.num_G2_pairs
+        self.min_pairs_multiple = 10
+
+        # set the number of other pairs to be the max # of combos or a multiple of the number of G2 pairs
+        self.num_G1_pairs = min(self.num_G1_pairs,self.min_pairs_multiple*self.num_G2_pairs)
+        self.num_G3_pairs = min(self.num_G3_pairs,self.min_pairs_multiple*self.num_G2_pairs)
+        self.num_G4_pairs = min(self.num_G4_pairs,self.min_pairs_multiple*self.num_G2_pairs)
+
+        # now create the sets for these pairs
+        self.G1_img_paths = []
+        self.G1_labels = []
+        self.G2_img_paths = []
+        self.G2_labels = []
+        self.G3_img_paths = []
+        self.G3_labels = []
+        self.G4_img_paths = []
+        self.G4_labels = []
+
+        self.s_sampler_idxs = [i for i in self.s_sampler] # [c0_idx, c1_idx, c2_idx, c0_idx, c2_idx, ...]
+        self.t_sampler_idxs = [i for i in self.t_sampler] # [c0_idx, c1_idx, c2_idx, c0_idx, c2_idx, ...]
+
+        # add G1 pairs
+        pair_cnt = 0
+        for i in range(0,len(self.s_sampler_idxs)): # iterate 1 by 1
+            for j in range(self.s_sampler.num_classes+i,len(self.s_sampler_idxs),self.s_sampler.num_classes): # iterate by n to get pairs
+                if i != j:
+                    x1_idx,x2_idx = self.s_sampler_idxs[i], self.s_sampler_idxs[j]
+                    self.G1_img_paths.append((self.source_dataset.imgs[x1_idx],self.source_dataset.imgs[x2_idx]))
+                    self.G1_labels.append(0) # class 0
+                    pair_cnt += 1
+                if pair_cnt >= self.num_G1_pairs:
+                    break
+            if pair_cnt >= self.num_G1_pairs:
+                break
+
+        # for i in range(self.num_G1_pairs)[0:10]:
+        #     x1_idx,x2_idx = s_sampler_idxs[i], s_sampler_idxs[i+self.s_sampler.num_classes]
+        #     self.G1_img_paths.append((self.source_dataset.imgs[x1_idx],self.source_dataset.imgs[x2_idx]))
+        #     self.G1_labels.append(0) # class 0
+
+        # for i in range(self.num_G2_pairs)[0:10]:
+        #     x1_idx,x2_idx = s_sampler_idxs[i], t_sampler_idxs[i+self.s_sampler.num_classes]
+        #     self.G1_img_paths.append((self.source_dataset.imgs[x1_idx],self.source_dataset.imgs[x2_idx]))
+        #     self.G1_labels.append(0) # class 0
+
+    # dataset size is number of images
+    def __len__(self):
+        return 0
+    
+    # how to get one sample from the dataset
+    def __getitem__(self, idx):
+        # attempt to load the image at the specified index
+        try:
+            img = Image.open(self.imgs[idx])
+            tt = torchvision.transforms.ToTensor()
+            tp = torchvision.transforms.ToPILImage()
+            tt_img = tt(img)
+            if(tt_img.size()[0] != 3):
+                img = tp(tt_img.repeat(3, 1, 1))
+            
+            # apply any transformation
+            if self.transform:
+                img = self.transform(img)
+            
+            # get the label
+            label = self.labels[idx]
+            
+            # return the sample (img (tensor)), object class (int), and the path optionally
+            return img, label, os.path.basename(self.imgs[idx])
+
+        # if the image is invalid, show the exception
+        except (ValueError, RuntimeWarning,UserWarning) as e:
+            print("Exception: ", e)
+            print("Bad Image: ", self.imgs[idx])
+            exit()
+    
+    # Diaply the results of a forward pass for a random batch of 64 samples
+    # if no model passed in, just display a batch with no predictions
+    def visualize_batch(self,model=None,device=None):
+        #import matplotlib
+        #matplotlib.use('TkAgg')
+
+        # create the dataloader
+        batch_size = 64
+        data_loader = DataLoader(self,batch_size,shuffle=True)
+
+        # get the first batch
+        (imgs, labels, paths) = next(iter(data_loader))
+        #(imgs, labels) = next(iter(data_loader))
+        imgs,labels = imgs.to(device), labels.to(device)
+        preds = None
+
+        # check if want to do a forward pass
+        if model != None:
+            preds = model(imgs)
+        
+        imgs,labels = imgs.to("cpu"), labels.to("cpu")
+        # display the batch in a grid with the img, label, idx
+        rows = 8
+        cols = 8
+        obj_classes = list(self.classes)
+        
+        fig,ax_array = plt.subplots(rows,cols,figsize=(20,20))
+        #fig.subplots_adjust(hspace=0.5)
+        plt.subplots_adjust(wspace=0, hspace=0)
+        for i in range(rows):
+            for j in range(cols):
+                idx = i*rows+j
+
+                # create text labels
+                text = str(labels[idx].item())
+                if model != None:
+                    text = "GT :" + obj_classes[labels[idx]]  + " P: ",obj_classes[preds[idx].argmax()]#", i=" +str(idxs[idx].item())
+                
+                # for normal forward pass use this line
+                #ax_array[i,j].imshow(imgs[idx].permute(1, 2, 0))
+
+                # for quantized forward pass use this line
+                #print(imgs[idx].size(),torch.min(imgs[idx]))
+                ax_array[i,j].imshow((imgs[idx].permute(1, 2, 0)+1)/2)
+
+                ax_array[i,j].set_title(text,color="white")
+                ax_array[i,j].set_xticks([])
+                ax_array[i,j].set_yticks([])
+        plt.savefig('plot.png')
+        #print(paths)
+        #plt.show()
+
+# ========================= custom data sampler ===========================
+class EvenSampler(Sampler):
+    def __init__(self, dataset,shot=-1):
+        # get the labels as a tensor
+        self.labels = torch.Tensor(dataset.labels)
+
+        # how many samples from each class to use
+        self.shot = shot
+
+        # count the number of classes
+        self.num_classes = len(torch.unique(self.labels))
+
+        # get the idxs of each class as a nested list --> [[0,1,2,3],[4,5,6]]
+        self.class_idxs = []
+        self.class_idx_lens = []
+        for c in range(self.num_classes):
+            # if we specify a shot then only use a subset of the samples per class
+            if self.shot != -1:
+                idxs = torch.flatten((self.labels == c).nonzero())[0:self.shot]
+            else:
+                idxs = torch.flatten((self.labels == c).nonzero())
+            self.class_idxs.append(idxs)
+            self.class_idx_lens.append(idxs.size(0))
+        self.max_len = max(self.class_idx_lens)
+        
+    def __iter__(self):
+        # shuffle the idxs for each class idx list --> [[1,0,2,3],[5,3,4]]
+        # also periodically extend the shorter lists --> [[1,0,2,3],[5,3,4,5]]
+        for i,c in enumerate(self.class_idxs):
+            rand_idx = torch.randperm(c.size(0))
+            shuffled = c[rand_idx]
+            self.class_idxs[i] = shuffled
+            added_len = self.max_len-shuffled.size(0)
+            self.class_idxs[i] = torch.cat((shuffled,shuffled[0:added_len]))
+
+        # interleave the shuffled lists so that every successive idx is a new class,
+        # to get even batches the batch size needs to be a multiple of the number of classes
+        # and we need an equal amount per class
+        zipped_idxs = torch.stack([t for t in self.class_idxs],dim=1)
+        return iter(zipped_idxs.view(zipped_idxs.numel()).tolist())
+    
+    def __len__(self):
+        return len(self.labels)
+
+
 
 
 # ================================= Helper functions for domain adaptation ===========================
@@ -354,7 +576,7 @@ def cats_and_dogs_get_datasets(data, load_train=True, load_test=True,apply_trans
             transforms.ToTensor(),
             ai8x.normalize(args=args)
         ])
-        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform,normalize=False)
+        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform)
 
     elif load_train and not apply_transforms:
         train_transform = transforms.Compose([
@@ -362,7 +584,7 @@ def cats_and_dogs_get_datasets(data, load_train=True, load_test=True,apply_trans
             transforms.ToTensor(),
             ai8x.normalize(args=args)
         ])
-        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform,normalize=False)
+        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform)
 
     else:
         train_dataset = None
@@ -375,7 +597,7 @@ def cats_and_dogs_get_datasets(data, load_train=True, load_test=True,apply_trans
             transforms.ToTensor(),
             ai8x.normalize(args=args)
         ])
-        test_dataset = ClassificationDataset(os.path.join(data_dir,"test"),test_transform,normalize=False)
+        test_dataset = ClassificationDataset(os.path.join(data_dir,"test"),test_transform)
 
     else:
         test_dataset = None
@@ -403,7 +625,7 @@ def imagenet10_get_datasets(data, load_train=True, load_test=True,apply_transfor
             transforms.ToTensor(),
             ai8x.normalize(args=args)
         ])
-        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform,normalize=False)
+        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform)
 
     elif load_train and not apply_transforms:
         train_transform = transforms.Compose([
@@ -411,7 +633,7 @@ def imagenet10_get_datasets(data, load_train=True, load_test=True,apply_transfor
             transforms.ToTensor(),
             ai8x.normalize(args=args)
         ])
-        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform,normalize=False)
+        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform)
 
     else:
         train_dataset = None
@@ -424,7 +646,7 @@ def imagenet10_get_datasets(data, load_train=True, load_test=True,apply_transfor
             transforms.ToTensor(),
             ai8x.normalize(args=args)
         ])
-        test_dataset = ClassificationDataset(os.path.join(data_dir,"test"),test_transform,normalize=False)
+        test_dataset = ClassificationDataset(os.path.join(data_dir,"test"),test_transform)
 
     else:
         test_dataset = None
@@ -452,7 +674,7 @@ def office5_get_datasets(data, load_train=True, load_test=True,apply_transforms=
             transforms.ToTensor(),
             ai8x.normalize(args=args)
         ])
-        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform,normalize=False)
+        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform)
 
     elif load_train and not apply_transforms:
         train_transform = transforms.Compose([
@@ -460,7 +682,7 @@ def office5_get_datasets(data, load_train=True, load_test=True,apply_transforms=
             transforms.ToTensor(),
             ai8x.normalize(args=args)
         ])
-        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform,normalize=False)
+        train_dataset = ClassificationDataset(os.path.join(data_dir,"train"),train_transform)
 
     else:
         train_dataset = None
@@ -473,7 +695,7 @@ def office5_get_datasets(data, load_train=True, load_test=True,apply_transforms=
             transforms.ToTensor(),
             ai8x.normalize(args=args)
         ])
-        test_dataset = ClassificationDataset(os.path.join(data_dir,"test"),test_transform,normalize=False)
+        test_dataset = ClassificationDataset(os.path.join(data_dir,"test"),test_transform)
 
     else:
         test_dataset = None
